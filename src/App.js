2fce6d62dd078d36b1505c40df3f2fb2f9e09134
harikanani <hkanani191@gmail.com>
2021-10-22 22:29:43 +0530

    fix route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Home from "./components/Home";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 import MyContext from "./context/MyContext";
